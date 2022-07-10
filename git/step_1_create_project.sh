21f5914d3e4e8c73baf07cfcf00fd272711a3f71
Warren R. Liske <61427748+liskewa@users.noreply.github.com>
2022-07-10 16:59:50 -0700

    Update step_1_create_project.sh

--- git/step_1_create_project.sh
+++ git/step_1_create_project.sh
@@ -1,10 +1,14 @@
+echo "Creating project"
 git init
-
 echo "CalTech DevOps Git Project" > Readme.md
 git add Readme.md
 git commit -m "Start of Git project"
 
+echo ""
+echo "Creating project branches"
 git branch HotFix
 git branch Integration
 
+echo ""
+echo "Listing existing branches"
 git branch -a
